427b9c943a0defd895054017e50adc020ae484ed
Nobuhiko Sawai <nobuhiko.sawai@gmail.com>
2020-09-12 14:27:26 +0900

    feat: add index

--- isuumo/webapp/mysql/db/0_Schema.sql
+++ isuumo/webapp/mysql/db/0_Schema.sql
@@ -17,7 +17,11 @@ CREATE TABLE isuumo.estate
     door_height INTEGER             NOT NULL,
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+    popularity  INTEGER             NOT NULL,
+    INDEX idx_features_popularity (features, popularity),
+    INDEX idx_rent_features_popularity (rent, features, popularity),
+    INDEX idx_door_width_door_height_popularity (door_width, door_height, popularity),
+    INDEX idx_latitude_longitude_popularity (latitude, longitude, popularity)
 );
 
 CREATE TABLE isuumo.chair
@@ -34,5 +38,13 @@ CREATE TABLE isuumo.chair
     features    VARCHAR(64)     NOT NULL,
     kind        VARCHAR(64)     NOT NULL,
     popularity  INTEGER         NOT NULL,
-    stock       INTEGER         NOT NULL
+    stock       INTEGER         NOT NULL,
+    INDEX idx_features_popularity_stock (features, popularity, stock),
+    INDEX idx_depth_features_popularity_stock (depth, features, popularity, stock),
+    INDEX idx_price_features_popularity_stock (price, features, popularity, stock),
+    INDEX idx_kind_popularity_stock(kind, popularity, stock),
+    INDEX idx_width_popularity_stock(width, popularity, stock),
+    INDEX idx_height_popularity_stock(height, popularity, stock),
+    INDEX idx_price_stock(price, stock),
+    INDEX idx_height_stock(height, stock)
 );
